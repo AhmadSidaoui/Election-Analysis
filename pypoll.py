e908410378a02952731a24abc77ea94f4e082666
ahmad sidaoui <ahmadsidaoui@ahmads-Air.home>
2023-03-15 16:24:50 +0100

    adding the election audit code.

--- pypoll.py
+++ pypoll.py
@@ -25,6 +25,12 @@ candidate_options = []
 
 candidate_votes = {}
 
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
 
     file_reader = csv.reader(election_data)
@@ -52,9 +58,25 @@ with open(file_to_load) as election_data:
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes)/float(total_votes) * 100
-        print(f"{candidate_name}: recieved {vote_percentage:.2f}% votes.\n")
-    
+        print(f"{candidate_name}: recieved {vote_percentage:.2f}% ({votes:,}).\n")
 
+        # Determing the winner
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    
+    winning_candidate_summary = (
+        f"-------------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning vote count: {winning_count:,}\n"
+        f"Winning percentage: {winning_percentage:.1f}\n"
+        f"-------------------------------\n"
+    )
+
+
+    print(winning_candidate_summary)
     print(f"Total number of votes: {total_votes:,}\n")
     print(f"The candidates that are running are:\n{candidate_options}\n")
     print(f"The number of votes each candidate has:\n{candidate_votes}\n")
